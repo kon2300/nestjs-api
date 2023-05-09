1eaafd5ee94d7494791a81aa68416ed394bbeae5
kon2300 <yyaamm.2300@gmail.com>
2023-05-09 17:48:58 +0900

    Fix: moduleの読み込み

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app.module';
-import { ValidationPipe } from '@nestjs/common';
 import { PrismaService } from '@/adaptor/primary/rdbms/prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.useGlobalPipes(new ValidationPipe());
 
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
